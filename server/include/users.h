723ed690c1a438db87b7f1a61fb6d235eb1ec93f
bsoto-fc <b.soto@ciencias.unam.mx>
2026-09-22 22:39:48 -0600

    Eliminar MessageSenderIterator de users.h.

--- server/include/users.h
+++ server/include/users.h
@@ -67,6 +67,4 @@ bool StartFirstTimeAuthentication(char* buffer, UserList* userList, int clientFD
 
 bool DisconnectUser(UserList* list, char* username, int clientFD); 
 
-bool MessageSenderIterator(const void* item, void* udata); 
-
 #endif // USERS_H_
